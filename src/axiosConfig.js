97e48fa2aee56255e28f44be052f2eeebcdcab9d
shagunn02 <greatshagun2005@gmail.com>
2025-07-15 17:39:21 +0530

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://tourmaline-gnome-b8c31e.netlify.app////api/users",
+  baseURL: "https://tourmaline-gnome-b8c31e.netlify.app/api/users",
   headers: {
     "Content-Type": "application/json",
   },
